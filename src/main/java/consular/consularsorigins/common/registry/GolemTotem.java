402cfc3a61a8215fda32dfa87b169de323d7388a
ConsularAtol <dennis21806@gmail.com>
2022-01-14 21:21:33 -0500

    Warden

--- src/main/java/consular/consularsorigins/common/registry/GolemTotem.java
+++
@@ -1,9 +0,0 @@
-package consular.consularsorigins.common.registry;
-
-import net.minecraft.item.ItemStack;
-
-public class GolemTotem {
-    public void appendTooltip(ItemStack itemStack, World world, List<Text> list, TooltipContext tooltipContext) {
-        list.add(new TranslatableText("classes.class.melee"));
-    }
-}
