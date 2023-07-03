c9cfd7d5a52887ba6cb85ec6bba53a09fab427a9
ConsularAtol <dennis21806@gmail.com>
2023-07-02 21:31:04 -0400

    1.20

--- src/main/java/consular/consularsorigins/common/ConsularsOrigins.java
+++ src/main/java/consular/consularsorigins/common/ConsularsOrigins.java
@@ -1,20 +1,20 @@
 package consular.consularsorigins.common;
 
-import consular.consularsorigins.common.network.packet.BoneMealPacket;
 import consular.consularsorigins.common.registry.ModConditions;
 import consular.consularsorigins.common.registry.ModItemGroups;
 import consular.consularsorigins.common.registry.ModPowers;
 import consular.consularsorigins.common.registry.ModScaleTypes;
 import consular.consularsorigins.common.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.util.Identifier;
 
 public class ConsularsOrigins implements ModInitializer {
 	public static final String MODID = "consularsorigins";
 
 	public static boolean isEnchantlingEnchanting;
-
+	public static boolean isKoboldMining;
+	public static boolean isAllayFlying;
+	
 	public static Identifier id(String path) {
         return new Identifier(MODID, path);
     }
@@ -26,7 +26,6 @@ public class ConsularsOrigins implements ModInitializer {
 		ModPowers.init();
 		ModConditions.init();
 		ModConditions.register();
-		ModItemGroups.registerItemGroups();
-		ServerPlayNetworking.registerGlobalReceiver(BoneMealPacket.ID, BoneMealPacket::receive);
+		//ModItemGroups.registerItemGroups();
 	};
 }
